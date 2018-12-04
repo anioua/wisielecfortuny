a5e193d9a1423394c3b4991113bd56a569493f06
unknown <anioua@gmail.com>
2018-12-04 22:45:05 +0100

    wisielec fortuny v1

---
+++ js/szubienica.js
@@ -0,0 +1,493 @@
+const tab=[];
+tab[0]=["Geografia","Ocean Atlantycki"];
+tab[1]=["Geografia","Wielka Rafa Koralowa"];
+tab[2]=["Geografia","Cieśnina Magellana"];
+tab[3]=["Geografia","Półwysep Apeniński"];
+tab[4]=["Geografia","Dolina Pięciu Stawów"];
+tab[5]=["Geografia","Wodospad Niagara"];
+tab[6]=["Geografia","Wędrówka Kontynentów"];
+tab[7]=["Geografia","Szerokość Geograficzna"];
+tab[8]=["Geografia","Tatrzański Park Narodowy"];
+tab[9]=["Słynna osoba","Ludwig van Beethoven"];
+tab[10]=["Słynna osoba","Albert Einstein"];
+tab[11]=["Słynna osoba","Napoleon Bonaparte"];
+tab[12]=["Słynna osoba","Leonardo Da Vinci"];
+tab[13]=["Słynna osoba","Michael Jackson"];
+tab[14]=["Słynna osoba","Archimedes z Syrakuz"];
+tab[15]=["Słynna osoba","Juliusz Cezar"];
+tab[16]=["Słynna osoba","Stanisław August Poniatowski"];
+tab[17]=["Słynna osoba","Maria Curie Skłodowska"];
+tab[18]=["Tytuł filmu","Indiana Jones i Ostatnia Krucjata"];
+tab[19]=["Tytuł filmu","Przeminęło z Wiatrem"];
+tab[20]=["Tytuł filmu","Gwiezdne Wojny Imperium Kontratakuje"];
+tab[21]=["Tytuł filmu","Świt Żywych Trupów"];
+tab[22]=["Tytuł filmu","Teksańska Masakra Piłą Mechaniczną"];
+tab[23]=["Tytuł filmu","Od Zmierzchu Do Świtu"];
+tab[24]=["Tytuł filmu","Piraci z Karaibów Klątwa Czarnej Perły"];
+tab[25]=["Tytuł filmu","Powrót do Przyszłości"];
+tab[26]=["Tytuł filmu","Kevin sam w domu"];
+tab[27]=["Architektura","Pałac Kultury i Nauki"];
+tab[28]=["Architektura","Katedra Notre Dame"];
+tab[29]=["Architektura","Sagrada Familia"];
+tab[30]=["Architektura","Pałac Kultury i Nauki"];
+tab[31]=["Architektura","Dolina Królów w Luksorze"];
+tab[32]=["Architektura","Statua Wolności"];
+tab[33]=["Architektura","Krzywa Wieża w Pizie"];
+tab[34]=["Architektura","Kaplica Sykstyńska"];
+tab[35]=["Architektura","Brama Brandenburska"];
+tab[36]=["Fauna","Motyl Paź Królowej"];
+tab[37]=["Fauna","Pstrąg Tęczowy"];
+tab[38]=["Fauna","Słoń Afrykański"];
+tab[39]=["Fauna","Niedzwiedź Brunatny"];
+tab[40]=["Fauna","Kobra Królewska"];
+tab[41]=["Fauna","Ropucha Aga"];
+tab[42]=["Fauna","Salamandra Plamista"];
+tab[43]=["Fauna","Rekin Wielorybi"];
+tab[44]=["Fauna","Zaskroniec Zwyczajny"];
+tab[45]=["Fauna","Mrówka Faraona"];
+tab[46]=["Flora","Fiołek Alpejski"];
+tab[47]=["Flora","Kapusta Pekińska"];
+tab[48]=["Flora","Trzcina Cukrowa"];
+tab[49]=["Flora","Fiołek Alpejski"];
+tab[50]=["Flora","Leszczyna"];
+tab[51]=["Flora","Koper Rzymski"];
+tab[52]=["Flora","Drzewo Sandałowe"];
+tab[53]=["Flora","Wierzba Płacząca"];
+tab[54]=["Flora","Muchomor Sromotnikowy"];
+tab[55]=["Fizyka i Chemia","Prawo Powszechnego Ciążenia"];
+tab[56]=["Fizyka i Chemia","Pierwsza Zasada Dynamiki Newtona"];
+tab[57]=["Fizyka i Chemia","Siła Odśrodkowa"];
+tab[58]=["Fizyka i Chemia","Prędkość Światła"];
+tab[59]=["Fizyka i Chemia","Ruch Jednostajnie Przyspieszony"];
+tab[60]=["Fizyka i Chemia","Perpetuum Mobile"];
+tab[61]=["Fizyka i Chemia","Fale Elektromagnetyczne"];
+tab[62]=["Fizyka i Chemia","Resublimacja"];
+tab[63]=["Fizyka i Chemia","Kwas Askorbinowy"];
+tab[64]=["Historia","Uchwalenie Konstytucji Trzeciego Maja"];
+tab[65]=["Historia","Wojny Napoleońskie"];
+tab[66]=["Historia","Wojna Secesyjna w USA"];
+tab[67]=["Historia","Dwudziestolecie Międzywojenne"];
+tab[68]=["Historia","Bitwa Pod Grunwaldem"];
+tab[69]=["Historia","Atak Na Pearl Harbor"];
+tab[70]=["Historia","Wojny Punickie"];
+tab[71]=["Historia","Zburzenie Muru Berlińskiego"];
+tab[72]=["Historia","Wyprawy Krzyżowe do Ziemi Świętej"];
+tab[73]=["Przysłowia i Powiedzenia","Nie ma tego złego co by na dobre nie wyszło"];
+tab[74]=["Przysłowia i Powiedzenia","Jak się nie ma co się lubi to się lubi co się ma"];
+tab[75]=["Przysłowia i Powiedzenia","Lepszy wróbel w garści niż gołąb na dachu"];
+tab[76]=["Przysłowia i Powiedzenia","Gdzie kucharek sześć tam nie ma co jeść"];
+tab[77]=["Przysłowia i Powiedzenia","Jedna jaskółka wiosny nie czyni"];
+tab[78]=["Przysłowia i Powiedzenia","Indyk myślał o niedzieli a w sobotę łeb ucięli"];
+tab[79]=["Przysłowia i Powiedzenia","Nie wszystko złoto, co się świeci"];
+tab[80]=["Przysłowia i Powiedzenia","Mówił dziad do obrazu a ten do niego ani razu"];
+tab[81]=["Przysłowia i Powiedzenia","Komu w drogę temu czas"];
+tab[82]=["Technika i Wynalazki","Rzeczywitość Rozszerzona"];
+tab[83]=["Technika i Wynalazki","Dzwięk Przestrzenny"];
+tab[84]=["Technika i Wynalazki","Okulary do Rzeczywistości Wirtualnej"];
+tab[85]=["Technika i Wynalazki","Silnik Odrzutowy"];
+tab[86]=["Technika i Wynalazki","Wzmacniacz Lampowy"];
+tab[87]=["Technika i Wynalazki","Maszyna Parowa"];
+tab[88]=["Technika i Wynalazki","Samochód Elektryczny"];
+tab[89]=["Technika i Wynalazki","Proch Strzelniczy"];
+tab[90]=["Technika i Wynalazki","Mikroskop Elektronowy"];
+tab[91]=["Astronomia","Układ Heliocentryczny"];
+tab[92]=["Astronomia","Wielki Obłok Magellana"];
+tab[93]=["Astronomia","Deszcz Meteorytów"];
+tab[94]=["Astronomia","Pierścienie Saturna"];
+tab[95]=["Astronomia","Trzecia Planeta od Słońca"];
+tab[96]=["Astronomia","Jednostka Astronomiczna"];
+tab[97]=["Astronomia","Lądowanie Człowieka na Księżycu"];
+tab[98]=["Astronomia","Zaćmienie Słońca"];
+tab[98]=["Astronomia","Koniunkcja Planet"];
+
+var losowaniePunktow = [1,2,3,5,50,100,125,150,200,250,300,500,750,1000,1250,1500,2000,2500,5000,"Bankrut"];
+
+losowanie=Math.round(98*Math.random());
+var losowaliczba=losowanie;
+
+var wylosowanehaslo=tab[losowaliczba][1];
+var kategoria=tab[losowaliczba][0];
+
+
+var haslo = wylosowanehaslo;
+haslo = haslo.toUpperCase();
+
+var dlugoschasla=haslo.length;
+var iloscskuch=0;
+var obrazszansa=1;
+var stankasy=0;
+var wylosowanePunkty = 0;
+var losowanieBlok = false;
+var kupowanieBlok = false;
+var bladBlok = false;
+var odgadnijBlok=false;
+
+var haslo1 = "";
+
+
+
+
+
+
+
+
+
+for (i=0; i<dlugoschasla; i++)
+{
+	if(haslo.charAt(i)==" ") haslo1=haslo1+" ";
+	else haslo1=haslo1+"-";
+}
+
+
+function wypisz_haslo()
+{
+	document.getElementById("plansza").innerHTML=haslo1;
+	document.getElementById("kategoria1").innerHTML=kategoria;
+}
+
+function portfel()
+{
+	stankasy=stankasy+wylosowanePunkty;
+	document.getElementById("menu").innerHTML='Punkty: '+stankasy;
+}
+
+function portfelSamogloska()
+{
+	if (stankasy>499)
+	{
+		stankasy=stankasy-500;
+		document.getElementById("menu").innerHTML='Punkty: '+stankasy;
+	}
+	if (stankasy<500)
+	{
+		document.getElementById("komunikaty").innerHTML='Masz za mało punktów, żeby kupić samogłoskę';
+	}
+}
+
+
+
+function bankrut1()
+{
+	stankasy=stankasy-stankasy;
+	document.getElementById("menu").innerHTML='Punkty: '+stankasy;
+	document.getElementById("wylosowana").innerHTML="Bankrut";
+}
+
+window.onload = start;
+
+var litery=new Array(35);
+
+litery[0]="B";
+litery[1]="C";
+litery[2]="Ć";
+litery[3]="D";
+litery[4]="F";
+litery[5]="G";
+litery[6]="H";
+litery[7]="J";
+litery[8]="K";
+litery[9]="L";
+litery[10]="Ł";
+litery[11]="M";
+litery[12]="N";
+litery[13]="Ń";
+litery[14]="P";
+litery[15]="Q";
+litery[16]="R";
+litery[17]="S";
+litery[18]="Ś";
+litery[19]="T";
+litery[20]="V";
+litery[21]="W";
+litery[22]="X";
+litery[23]="Z";
+litery[24]="Ź";
+litery[25]="Ż";
+
+var literySamogloski=new Array(9);
+
+literySamogloski[0]="A";
+literySamogloski[1]="Ą";
+literySamogloski[2]="E";
+literySamogloski[3]="Ę";
+literySamogloski[4]="I";
+literySamogloski[5]="O";
+literySamogloski[6]="Ó";
+literySamogloski[7]="U";
+literySamogloski[8]="Y";
+
+
+function start()
+{
+	
+	portfel();
+	var tresc_diva= "";
+	var tresc_diva1= "";
+	document.getElementById("komunikaty").innerHTML="Zakręć kołem."; document.getElementById("komunikaty").style.color="lime";
+
+	var odgadnij  = document.getElementById('przyciskOdgadnijHaslo');	
+	odgadnij.addEventListener("click", function() { odgadnijHaslo();});
+	
+	function odgadnijHaslo(){
+		
+	  if (odgadnijBlok==false){
+		  
+		  
+		  	document.getElementById("przyciskOdgadnijHaslo").innerHTML="Powrót";
+		  	document.getElementById("odgadywanie").style.visibility="hidden";
+		  	document.getElementById("sprawdzHaslo").style.visibility="visible";
+
+
+
+		  odgadnijBlok=true;
+	  }else {
+			
+			document.getElementById("przyciskOdgadnijHaslo").innerHTML="Zgaduję hasło";
+
+			document.getElementById("odgadywanie").style.visibility="visible";
+		  	document.getElementById("sprawdzHaslo").style.visibility="hidden";
+				
+			odgadnijBlok=false;
+		}
+
+	}
+	
+	
+
+	var zakrec0  = document.getElementById('zakrec');	
+	zakrec0.addEventListener("click", function() { losujPunkty();});
+
+	
+	function losujPunkty(){
+			if (losowanieBlok==false){
+				losowanieBlok=true;
+		var wynik = Math.floor(Math.random() * (19 + 1));
+
+var w = 0;    
+var czas = 50;                 
+
+function myLoop () {           
+document.getElementById("wylosowana").style="clear:both";
+document.getElementById("komunikaty").innerHTML="";
+   setTimeout(function () { 
+	 czas= czas + 25;
+	if (w==wynik){
+		kupowanieBlok=true;
+
+		if (wynik==19) {
+			bankrut1();
+			losowanieBlok=false;
+			wylosowanePunkty1 = losowaniePunktow[w];
+		document.getElementById("wylosowana").innerHTML=wylosowanePunkty1;
+		document.getElementById("wylosowana").style.background="#003300";
+		document.getElementById("wylosowana").style.color="gold";
+		document.getElementById("wylosowana").style.borderRadius="5px";
+		document.getElementById("wylosowana").style.boxShadow="0 0 5px 5px gold";
+		document.getElementById("komunikaty").innerHTML="Straciłeś wszystkie punkty, zakręć jeszcze raz.";
+			return;
+		}
+			bladBlok=false;
+			losowanieBlok=true;
+		wylosowanePunkty1 = losowaniePunktow[w];
+		document.getElementById("wylosowana").innerHTML=wylosowanePunkty1;
+		document.getElementById("wylosowana").style.background="#003300";
+		document.getElementById("wylosowana").style.color="gold";
+		document.getElementById("wylosowana").style.borderRadius="5px";
+		document.getElementById("wylosowana").style.boxShadow="0 0 5px 5px gold";
+		if (stankasy>499){document.getElementById("komunikaty").innerHTML="Zgadnij spółgłoskę lub kup samogłoskę."; document.getElementById("komunikaty").style.color="lime";}
+		if (stankasy<500){document.getElementById("komunikaty").innerHTML="Zgadnij spółgłoskę."; document.getElementById("komunikaty").style.color="lime";}
+	}
+    if (w<wynik) {
+		
+		wylosowanePunkty1 = losowaniePunktow[w];
+		document.getElementById("wylosowana").innerHTML=wylosowanePunkty1;
+		w++;                             
+		myLoop();             
+	}                   
+   }, czas)
+}
+
+myLoop();		
+		
+		wylosowanePunkty = losowaniePunktow[wynik];
+
+			}
+		
+		}
+		
+
+	
+	for(i=0; i<=25; i++)
+	{
+		var element="lit"+i;
+
+		tresc_diva= tresc_diva+'<div class="litera" onclick="sprawdz('+i+','+0+')" id="'+element+'">'+litery[i]+'</div>';
+		if((i+1)%13==0)tresc_diva=tresc_diva+'<div style="clear:both;"></div>'
+		
+	}
+
+	for(i=0; i<=8; i++)
+	{
+		var element="litSamo"+i;
+
+		tresc_diva1= tresc_diva1+'<div class="litera" onclick="sprawdz('+i+','+1+')" id="'+element+'">'+literySamogloski[i]+'</div>';
+		
+		
+	}
+	
+	document.getElementById("alfabet").innerHTML=tresc_diva;
+	document.getElementById("alfabetSamogloski").innerHTML=tresc_diva1;
+	
+	document.getElementById("alfabet2").innerHTML=tresc_diva;
+	document.getElementById("alfabetSamogloski2").innerHTML=tresc_diva1;
+	
+	wypisz_haslo();
+			document.getElementById("plansza2").innerHTML=haslo1;
+}
+
+String.prototype.ustawZnak=function(miejsce,znak)
+{
+	if(miejsce>this.length-1)return this.toString();
+	else return this.substr(0,miejsce)+znak+this.substr(miejsce+1);  
+}
+
+function sprawdz(nr,spolgloskaSamogloska)
+{
+
+
+	var trafiona=false;
+
+	
+	if ((losowanieBlok==true ) && (kupowanieBlok==true)){
+		
+		var kupnoSamogloski=false;
+		
+	for(i=0; i<dlugoschasla; i++)
+	{
+		if ((haslo.charAt(i)==litery[nr]) && (spolgloskaSamogloska==0))
+		{
+			haslo1=haslo1.ustawZnak(i,litery[nr]);
+			trafiona=true;
+			portfel();
+		}
+
+	}
+	
+	if(trafiona==true)
+	{  
+		if (spolgloskaSamogloska==0){
+		var element="lit"+nr;
+		document.getElementById(element).style.background="#003300";
+		document.getElementById(element).style.color="#00C000";
+		document.getElementById(element).style.border="3px solid #00C000";
+		document.getElementById(element).style.cursor="default";
+		wypisz_haslo(); 
+		if (stankasy>499){document.getElementById("komunikaty").innerHTML="Zakręć kołem lub kup samogłoskę."; document.getElementById("komunikaty").style.color="lime";}
+		if (stankasy<500){document.getElementById("komunikaty").innerHTML="Zakręć kołem."; document.getElementById("komunikaty").style.color="lime";}
+		losowanieBlok=false;
+		kupowanieBlok = false;}
+		
+
+	}
+	else
+	{	
+
+		if (spolgloskaSamogloska==0){
+		var element="lit"+nr;
+		document.getElementById(element).style.background="#330000";
+		document.getElementById(element).style.color="#C0000";
+		document.getElementById(element).style.border="3px solid #C00000";
+		document.getElementById(element).style.cursor="default";
+		document.getElementById(element).setAttribute("onclick",";");
+		losowanieBlok=false;
+		iloscskuch++;
+		var obraz="img/blad"+iloscskuch+".jpg";
+		document.getElementById("szubienica"+iloscskuch).innerHTML='<img src="'+obraz+'"alt=""/>';
+		document.getElementById("komunikaty").innerHTML="Zakręć kołem"; document.getElementById("komunikaty").style.color="lime";
+		kupowanieBlok = false;
+		}		
+	}
+	}
+	
+	if ((stankasy>499) && (bladBlok==false)){
+		
+		var kupnoSamogloski=false;
+		
+	for(i=0; i<dlugoschasla; i++)
+	{
+		
+
+		if ((haslo.charAt(i)==literySamogloski[nr]) && (spolgloskaSamogloska==1) && (stankasy>499))
+		{
+			
+			haslo1=haslo1.ustawZnak(i,literySamogloski[nr]);
+			trafiona=true;
+			if (kupnoSamogloski==false){ portfelSamogloska();}
+			kupnoSamogloski=true;
+
+		}
+	}
+	
+	if(trafiona==true)
+	{  
+		
+		if (spolgloskaSamogloska==1){
+		var element="litSamo"+nr;
+		document.getElementById(element).style.background="#003300";
+		document.getElementById(element).style.color="#00C000";
+		document.getElementById(element).style.border="3px solid #00C000";
+		document.getElementById(element).style.cursor="default";
+		wypisz_haslo();
+		if (stankasy>499){document.getElementById("komunikaty").innerHTML="Zakręć kołem lub kup samogłoskę."; document.getElementById("komunikaty").style.color="lime";}
+		if (stankasy<500){document.getElementById("komunikaty").innerHTML="Zakręć kołem."; document.getElementById("komunikaty").style.color="lime";}
+		losowanieBlok=false;
+		kupowanieBlok = false;}
+	}
+	else
+	{	
+
+
+
+		if ((spolgloskaSamogloska==1) && (stankasy>499)){
+		var element="litSamo"+nr;
+		document.getElementById(element).style.background="#330000";
+		document.getElementById(element).style.color="#C0000";
+		document.getElementById(element).style.border="3px solid #C00000";
+		document.getElementById(element).style.cursor="default";
+		document.getElementById(element).setAttribute("onclick",";");
+		losowanieBlok=false;
+		iloscskuch++;
+		var obraz="img/blad"+iloscskuch+".jpg";
+		document.getElementById("szubienica"+iloscskuch).innerHTML='<img src="'+obraz+'"alt=""/>';
+		document.getElementById("komunikaty").innerHTML="Zakręć kołem"; document.getElementById("komunikaty").style.color="lime";
+		kupowanieBlok = false;
+		bladBlok = true;
+		}				
+	}	
+	}
+	
+			document.getElementById("plansza2").innerHTML=haslo1;
+
+	//wygrana
+	if(haslo==haslo1)
+	document.getElementById("alfabet").innerHTML="<br /><br />Gratulacje!! <br />Odgadłeś Hasło!!: "+haslo+
+	'<br />Zdobyłeś :'+stankasy+' punktów<br /><br /><span class="reset"onclick="location.reload()">Jeszcze raz?</span>'
+	
+	
+	//przegrana
+	if(iloscskuch>=5)
+	document.getElementById("alfabet").innerHTML="<br /><br />Przegrana :((  <br />Prawidłowe hasło to: "+haslo+
+	'<br /><br /><span class="reset"onclick="location.reload()">Jeszcze raz?</span>'	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
